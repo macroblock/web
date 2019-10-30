195ec637b8c167eaa0f166b3da55fdb70ebf888f
macroblock <undefined.subject@gmail.com>
2019-10-30 20:56:56 +1000

    work in progress

--- cmd/ftpchecksrv/main.go
+++ cmd/ftpchecksrv/main.go
@@ -7,7 +7,10 @@ import (
 	"net/url"
 	"os"
 	"strconv"
+	"sync"
+	"time"
 
+	"github.com/macroblock/cpbftpchk/xftp"
 	"github.com/macroblock/imed/pkg/cli"
 	"github.com/macroblock/imed/pkg/misc"
 	"github.com/macroblock/imed/pkg/zlog/loglevel"
@@ -22,89 +25,124 @@ var (
 	flagTest   bool
 	flagSource string
 
+	urlStruct *url.URL
+	globList  []xftp.TEntry
+
 	cwd string
+
+	refreshTime     time.Time
+	refreshInterval time.Duration = 30 * time.Second
+	retryInterval   time.Duration = 5 * time.Second
+
+	mtx sync.Mutex
 )
 
 func mainHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func listHandler(w http.ResponseWriter, r *http.Request) {
-	if flagTest {
-		jsonData, err := json.Marshal(testdata.Data)
-		if err != nil {
-			panic(err)
-		}
-		w.Header().Set("content-type", "application/json")
-		_, err = w.Write(jsonData)
-		if err != nil {
-			panic(err)
-		}
-		return
+
+	mtx.Lock()
+	data := globList
+	mtx.Unlock()
+
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		panic(err)
 	}
+	w.Header().Set("content-type", "application/json")
+	_, err = w.Write(jsonData)
+	if err != nil {
+		panic(err)
+	}
+
 	return
 }
 
-func checkURL(s string) (*url.URL, error) {
-	u, err := url.Parse(flagSource)
+func reloadList(urlStruct *url.URL) error {
+
+	if flagTest {
+		mtx.Lock()
+		globList = testdata.Data
+		mtx.Unlock()
+		return nil
+	}
+
+	ftp, err := xftp.New2(urlStruct)
 	if err != nil {
-		return nil, fmt.Errorf("Parse url error: %v", err)
+		return err
+	}
+	defer ftp.Quit()
+
+	list, err := ftp.List(urlStruct.Path)
+	if err != nil {
+		return err
 	}
 
-	// if u.Path != "" {
-	// 	u.Path = "/" + u.Path
-	// }
+	mtx.Lock()
+	globList = list
+	mtx.Unlock()
 
-	// if u.Scheme == "" {
-	// 	switch u.Port() {
-	// 	case "21":
-	// 		u.Scheme = "ftp"
-	// 	case "22":
-	// 		u.Scheme = "sftp"
-	// 	}
-	// }
-	// if u.Port() == "" {
-	// 	switch u.Scheme {
-	// 	case "":
-	// 		u.Scheme = "ftp"
-	// 		u.Host = u.Hostname() + ":21"
-	// 	case "ftp":
-	// 		u.Host = u.Hostname() + ":21"
-	// 	case "sftp":
-	// 		u.Host = u.Hostname() + ":22"
-	// 	}
-	// }
-
-	if u.Host == "" {
-		return nil, fmt.Errorf("host name is empty")
-	}
-
-	// if cs.Password == "" && cs.Username != "" {
-	// 	fmt.Println("Enter Password:")
-	// 	bytePassword, err := terminal.ReadPassword(int(syscall.Stdin))
-	// 	if err != nil {
-	// 		return nil, err
-	// 	}
-	// 	// fmt.Println("\nPassword typed: " + string(bytePassword))
-	// 	cs.Password = string(bytePassword)
-	// }
-	return u, nil
+	return nil
 }
 
-func mainFunc() error {
-	u, err := checkURL(flagSource)
-	_ = u
-	if err != nil {
-		return fmt.Errorf("check URL error: %q", err)
-	}
+func ftpProcess(urlStruct *url.URL) {
+	go func() {
+		for {
+			fmt.Printf("--- loop ---\n")
 
+			diff := time.Since(refreshTime)
+			if diff < refreshInterval {
+				time.Sleep(refreshInterval - diff)
+				continue
+			}
+
+			err := reloadList(urlStruct)
+			if err != nil {
+				fmt.Printf("Error: %v\n", err)
+				time.Sleep(retryInterval)
+				continue
+			}
+			// for i, v := range globList {
+			// 	fmt.Printf("%3d - %v\n", i, v.Name)
+			// }
+
+			refreshTime = time.Now()
+		}
+	}()
+}
+
+func conformFlags() error {
 	port, err := strconv.Atoi(flagPort)
 	if err != nil || port < 0 {
 		return fmt.Errorf("Invalid port number: %q", flagPort)
 	}
 	flagPort = ":" + strconv.Itoa(port)
 
-	mux := http.NewServeMux()
+	if flagTest {
+		return nil
+	}
 
+	urlStruct, err = url.Parse(flagSource)
+	if err != nil {
+		return fmt.Errorf("Parse url error: %v", err)
+	}
+	if urlStruct.Host == "" {
+		return fmt.Errorf("host name is empty")
+	}
+	return nil
+}
+
+func mainFunc() error {
+	err := conformFlags()
+	if err != nil {
+		return err
+	}
+
+	// fmt.Printf("url struct:\n%v\n", urlStruct)
+	ftpProcess(urlStruct)
+
+	mux := http.NewServeMux()
 	mux.HandleFunc("/", mainHandler)
 	mux.HandleFunc("/list", listHandler)
 
@@ -130,14 +168,14 @@ func main() {
 	// }()
 
 	// command line interface
-	cmdLine := cli.New("!PROG! test server.", mainFunc)
+	cmdLine := cli.New("!PROG! is a test server.", mainFunc)
 	cmdLine.Elements(
 		cli.Usage("!PROG! {flags|<...>}"),
 		// cli.Hint("Use '!PROG! help <flag>' for more information."),
-		cli.Flag("-h --help   : help", cmdLine.PrintHelp).Terminator(), // Why is this works ?
+		cli.Flag("-h --help   : help", cmdLine.PrintHelp).Terminator(), // Why does this work ?
 		cli.Flag("-p --port   : port to listen.", &flagPort),
 		cli.Flag("-t --test   : test mode on.", &flagTest),
-		cli.Flag("-s --sourse : [proto://][username[:password]@]host[/path][:port]", &flagSource),
+		cli.Flag("-s --source : [proto://][username[:password]@]host[/path][:port]", &flagSource),
 		cli.OnError("Run '!PROG! -h' for usage.\n"),
 	)
 
--- cmd/ftpchecksrv/testdata/testdata.go
+++ cmd/ftpchecksrv/testdata/testdata.go
@@ -9,7 +9,7 @@ import (
 // Data -
 var Data = []xftp.TEntry{
 	{Name: "hd_1998_Dzhia__q0w0_ar2_xut1IeBPleZ_film.mp4", Size: 21619195066, Time: time.Now(), Type: xftp.File},
-	{Name: "hd_2009_Serye_sady__q0w0_xTgQwvCkqrR_film.mp4", Size: 24034176119, Time: time.Now(), Type: xftp.File},
+	{Name: "hd_2009_Serye_sady__q0w0_xTgQwvCkqrR_film.mp4.part", Size: 24034176119, Time: time.Now(), Type: xftp.File},
 	{Name: "hd_2019_Hristiane_kak_my_s01_02__q0w0_ar2_xez2JAhc34U_film.mp4", Size: 9304393143, Time: time.Now(), Type: xftp.File},
 	{Name: "hd_2019_Na_chasti_razdeleny_na_granice__q0w0_ar6e2_xmr6CbJ2UJg_film.mkv", Size: 7473251353, Time: time.Now(), Type: xftp.File},
 	{Name: "hd_2019_Na_chasti_razdeleny_na_granice__q0w0_trailer.mp4", Size: 307977131, Time: time.Now(), Type: xftp.File},
